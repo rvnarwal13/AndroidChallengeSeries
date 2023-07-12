2bc2fe277c4903211f91fc532e41ab2a4520a464
rvnarwal13 <rvnarwal13@gmail.com>
2023-07-12 08:37:46 +0530

    Day 2 - EditText and TextView

--- MyApplication1/app/src/main/java/com/ravi/myapplication1/MainActivity.java
+++ MyApplication1/app/src/main/java/com/ravi/myapplication1/MainActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private TextView textView;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +20,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.btn_click_me);
+        textView = findViewById(R.id.tv_display_text);
+        editText = findViewById(R.id.et_input_field);
         
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+                displayText();
             }
         });
+    }
+
+    private void displayText() {
+        String text = editText.getText().toString();
+        textView.setText(text);
     }
 }
 
